3a86ba239b890b615cea6f49039f0cf6e2cfe49b
BojanVr <bojanvrskovski@gmail.com>
2023-09-02 13:43:33 +0200

    added the readAllPets method in the controller

--- src/main/java/com/petstore/controller/PetController.java
+++ src/main/java/com/petstore/controller/PetController.java
@@ -1,12 +1,16 @@
 package com.petstore.controller;
 
 import com.petstore.entity.Pet;
+import com.petstore.entity.dto.PetDto;
 import com.petstore.entity.request.PetRequest;
 import com.petstore.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class PetController {
@@ -20,5 +24,10 @@ public class PetController {
 	@MutationMapping
 	public Pet createPet(@Argument PetRequest petRequest){
 		return petService.createPet(petRequest);
+	}
+
+	@QueryMapping
+	public List<PetDto> readAllPets(){
+		return petService.readAllPets();
 	}
 }

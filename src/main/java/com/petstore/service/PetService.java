6ab9800fe07ab8ba59060d6464f13b63478f1188
BojanVr <bojanvrskovski@gmail.com>
2023-09-03 09:56:39 +0200

    added the buy method in the service

--- src/main/java/com/petstore/service/PetService.java
+++ src/main/java/com/petstore/service/PetService.java
@@ -9,4 +9,5 @@ import java.util.List;
 public interface PetService {
 	Pet createPet(PetRequest petRequest);
 	List<PetDto> readAllPets();
+	Boolean buy(Long userId,Long petId);
 }
--- src/main/java/com/petstore/service/impl/PetServiceImpl.java
+++ src/main/java/com/petstore/service/impl/PetServiceImpl.java
@@ -1,13 +1,14 @@
 package com.petstore.service.impl;
 
 import com.petstore.entity.Pet;
+import com.petstore.entity.User;
 import com.petstore.entity.dto.PetDto;
 import com.petstore.entity.enums.PetType;
 import com.petstore.entity.request.PetRequest;
 import com.petstore.exception.NoPetsFoundException;
-import com.petstore.exception.NoUsersFoundException;
 import com.petstore.exception.PetAlredyExistsException;
 import com.petstore.repository.PetRepository;
+import com.petstore.repository.UserRepository;
 import com.petstore.service.PetService;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
@@ -19,17 +20,20 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class PetServiceImpl implements PetService {
 	private static final Logger log = LoggerFactory.getLogger(PetServiceImpl.class);
 	private final PetRepository petRepository;
+	private final UserRepository userRepository;
 	private final ModelMapper modelMapper;
 
 	@Autowired
-	public PetServiceImpl(PetRepository petRepository, ModelMapper modelMapper) {
+	public PetServiceImpl(PetRepository petRepository, UserRepository userRepository, ModelMapper modelMapper) {
 		this.petRepository = petRepository;
+		this.userRepository = userRepository;
 		this.modelMapper = modelMapper;
 	}
 
@@ -78,5 +82,44 @@ public class PetServiceImpl implements PetService {
 		return pets.stream()
 		            .map(pet -> modelMapper.map(pet, PetDto.class))
 		            .collect(Collectors.toList());
+	}
+
+	@Override
+	public Boolean buy(Long userId, Long petId) {
+		Optional<User> userOptional = userRepository.findById(userId);
+		Optional<Pet> petOptional = petRepository.findById(petId);
+
+		if (userOptional.isEmpty() || petOptional.isEmpty()) {
+			return false;
+		}
+
+		User user = userOptional.get();
+		Pet pet = petOptional.get();
+
+		if (pet.getOwner() != null) {
+			return false;
+		}
+
+		BigDecimal userBudget = user.getBudget();
+		BigDecimal petPrice = pet.getPrice();
+
+		if (userBudget.compareTo(petPrice) >= 0) {
+			user.setBudget(userBudget.subtract(petPrice));
+
+			pet.setOwner(user.getUserId());
+
+			userRepository.save(user);
+			petRepository.save(pet);
+
+			if (pet.getPetType() == PetType.CAT) {
+				System.out.println("Meow, cat " + pet.getName() + " has owner " + user.getFirstName());
+			} else if (pet.getPetType() == PetType.DOG) {
+				System.out.println("Woof, dog " + pet.getName() + " has owner " + user.getFirstName());
+			}
+
+			return true;
+		} else {
+			return false;
+		}
 	}
 }

5912caa68858a0a38a7a01db02456b68fcc63f95
BojanVr <bojanvrskovski@gmail.com>
2023-09-09 09:23:40 +0200

    added contstant for the user

---
+++ src/test/java/com/petstore/util/UserConstants.java
@@ -0,0 +1,12 @@
+package com.petstore.util;
+
+import java.math.BigDecimal;
+
+public class UserConstants {
+	public static final Long userId = 1L;
+	public static final String firstName = "Bojan";
+	public static final String lastName = "Vrshkovski";
+	public static final String emailAddress = "bojan@gmail.com";
+	public static final BigDecimal budget = new BigDecimal(2000);
+	
+}
